e6b711fb94ba1d128366e9f006def71fa080beac
Menyueru <maddog28@gmail.com>
2014-05-15 16:41:33 -0400

    Pushing new changes

---
+++ source/app.specs/LabelGeneratorSpec.cs
@@ -0,0 +1,36 @@
+﻿using app.web.core;
+using app.utility;
+using developwithpassion.specifications.extensions;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+using System.Collections.Generic;
+
+namespace app.specs
+{
+    class LabelGeneratorSpec
+    {
+        public abstract class concern : Observes<LabelGenerator>
+        {
+        }
+        public class when_creating_a_label : concern
+        {
+            Establish c = () =>
+                {
+                    result=new List<string>();
+                    depends.on<IList<string>>(new List<string> {"a","b","c","d","e","f"});
+                };
+            Because b = () =>
+            {
+                result.Add(sut.create_label());
+            };
+            
+            It creates_a_label = () =>
+                result[0].ShouldMatch("a");
+
+
+           
+
+            static List<string> result;
+        }
+    }
+}
---
+++ source/app/utility/LabelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace app.utility
+{
+    public class LabelGenerator
+    {
+        int label_limit;
+        IList<char> vocabulary;
+        int sequence;
+
+        public LabelGenerator(IList<char> vocabulary, int label_limit)
+        {
+            this.vocabulary = vocabulary;
+            this.label_limit = label_limit;
+            this.sequence = 0;
+        }
+
+        public string create_label()
+        {
+            string label="";
+            int modulo=sequence;
+            int length = vocabulary.Count();
+            do
+            {
+                label+=vocabulary[modulo%length];
+                modulo /= length;
+            }
+            while(modulo>0);
+            sequence++;
+            return label;
+        }
+    }
+}
