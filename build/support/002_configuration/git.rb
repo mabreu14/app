6f7f10d3312189ea27401682ebfcb819f9d6998a
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-15 08:34:22 -0600

    Updated

--- build/support/002_configuration/git.rb
+++ build/support/002_configuration/git.rb
@@ -2,7 +2,7 @@ configs ={
   :git => {
     :provider => 'github',
     :user => 'intellisysmay2014',
-    :remotes => potentially_change("remotes",__FILE__),
+    :remotes => %w/mabreu14 adnanjt juanmcastillo gadielreyes 040mike Menyueru/,
     :repo => 'app' 
   }
 }
