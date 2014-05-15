ea37a68baaeb999842b3de3532f0aa4289646a22
Menyueru <maddog28@gmail.com>
2014-05-15 17:11:28 -0400

    Pushing new changes

--- source/app/utility/LabelGenerator.cs
+++ source/app/utility/LabelGenerator.cs
@@ -14,18 +14,17 @@ namespace app.utility
     {
       this.vocabulary = vocabulary;
       this.label_limit = label_limit;
-      this.sequence = 0;
+      this.sequence = 1;
     }
 
     public string create_label(int number)
     {
-      var modulo = sequence;
       var length = vocabulary.Count();
       var index = number - 1;
 
       if (index < length)
         return vocabulary[index];
-      return create_label() + create_label(??)
+      return create_label(number % length) + create_label(number / length);
     }
 
     IEnumerator IEnumerable.GetEnumerator()
@@ -37,7 +36,7 @@ namespace app.utility
     {
       while (sequence < label_limit)
       {
-        yield return create_label();
+        yield return create_label(sequence);
         sequence++;
       }
     }
