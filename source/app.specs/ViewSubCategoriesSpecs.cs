cbae171d2df93e16d80b210b5c10f3d2b33a7651
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-16 07:16:33 -0600

    Updated

--- source/app.specs/ViewSubCategoriesSpecs.cs
+++ source/app.specs/ViewSubCategoriesSpecs.cs
@@ -1,26 +1,22 @@
-﻿ using app.web.application.store_browsing;
- using app.web.core;
- using Machine.Specifications;
- using developwithpassion.specifications.rhinomocks;
- using developwithpassion.specifications.extensions;
+﻿using app.web.application.store_browsing;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
 
 namespace app.specs
-{  
-  [Subject(typeof(ViewSubCategories))]  
+{
+  [Subject(typeof(ViewSubCategories))]
   public class ViewSubCategoriesSpecs
   {
     public abstract class concern : Observes<ISupportAUserFeature,
       ViewSubCategories>
     {
-        
     }
 
-   
     public class when_run : concern
     {
       It first_observation = () =>
       {
-
       };
     }
   }
