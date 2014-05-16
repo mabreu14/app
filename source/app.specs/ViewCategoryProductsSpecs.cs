3bc934a404a8bb6e5388d3c80ade4a9df4a0fb7d
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2014-05-16 09:31:08 -0600

    Updated

---
+++ source/app.specs/ViewCategoryProductsSpecs.cs
@@ -0,0 +1,23 @@
+﻿using app.web.application.store_browsing;
+using app.web.core;
+using developwithpassion.specifications.rhinomocks;
+using Machine.Specifications;
+
+namespace app.specs
+{
+  [Subject(typeof(ViewCategoryProducts))]
+  public class ViewCategoryProductsSpecs
+  {
+    public abstract class concern : Observes<ISupportAUserFeature,
+      ViewCategoryProducts>
+    {
+    }
+
+    public class when_run : concern
+    {
+      It first_observation = () =>
+      {
+      };
+    }
+  }
+}
